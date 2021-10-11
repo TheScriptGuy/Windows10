286a5b53a5b2bd7f7a3b84a8ffed7156456abff3
Nolan <github@remotenode.org>
2021-10-11 16:32:23 -0700

    Update InstallFireFox.ps1

--- InstallFireFox.ps1
+++ InstallFireFox.ps1
@@ -1,6 +1,8 @@
 # Download and install FireFox
+# Author:             TheScriptGuy (https://github.com/TheScriptGuy)
 # Last updated:       2021/10/11
-# Version: 0.01
+# Version:            0.01
+
 
 $SourceURL = "https://download.mozilla.org/?product=firefox-msi-latest-ssl&os=win64&lang=en-US";
 
