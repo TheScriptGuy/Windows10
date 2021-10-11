e069bf23b53a6938c654166b771c9501a4fcccc6
Nolan <github@remotenode.org>
2021-10-11 16:31:45 -0700

    Updating file to fix install errors.

--- InstallFireFox.ps1
+++ InstallFireFox.ps1
@@ -1,10 +1,13 @@
 # Download and install FireFox
+# Last updated:       2021/10/11
+# Version: 0.01
+
 $SourceURL = "https://download.mozilla.org/?product=firefox-msi-latest-ssl&os=win64&lang=en-US";
 
-$Installer = $env:TEMP + "\firefox.exe"; 
+$Installer = $env:TEMP + "\firefox.msi"; 
 
 Invoke-WebRequest $SourceURL -OutFile $Installer;
 
-Start-Process -FilePath $Installer -Args "/s" -Verb RunAs -Wait; 
+Start-Process msiexec.exe -Wait -ArgumentList "/i $Installer /qn" -Verb RunAs; 
 
 Remove-Item $Installer;
