f306668a55a31642b52bf18da37ab04ed542ce9a
Nolan <github@remotenode.org>
2022-01-12 15:36:18 -0800

    Add files via upload

---
+++ Install-Microsoft-Edge.ps1
@@ -0,0 +1,4 @@
+md -Path $env:temp\edgeinstall -erroraction SilentlyContinue | Out-Null
+$Download = join-path $env:temp\edgeinstall MicrosoftEdgeEnterpriseX64.msi
+Invoke-WebRequest 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/a2662b5b-97d0-4312-8946-598355851b3b/MicrosoftEdgeEnterpriseX64.msi'  -OutFile $Download
+Start-Process "$Download" -ArgumentList "/quiet"
